52534ad7d88db606a0993558a0d88a000d4caae0
bolstad <bolstad@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-11-09 15:19:52 +0000

    A new package providing microarray related methods that utilze BufferedMatrix objects
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BufferedMatrixMethods@21056 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/BM_Preprocess.R
@@ -0,0 +1,72 @@
+###
+###
+### file: BM_Preprocess.R
+###
+### aim: Pre-procesing functions applied to BufferedMatrix objects
+###
+### Copyright (C) 2006 B. M. Bolstad
+###
+### Written by; B. M. Bolstad <bmb@bmbolstad.com>
+###
+### History: 
+### June 27, 2006 - Initial version including support only for quantile normalization
+### Aug 9, 2006 - add in the RMA background correction
+### Aug 10, 2006 - add in median polish summarization
+
+
+
+normalize.BufferedMatrix.quantiles <- function(x,copy=TRUE){
+
+  if (!is(x,"BufferedMatrix")){
+    stop("Need BufferedMatrix")
+  }
+
+  if (copy){
+    x.copy <- duplicate(x)
+  } else {
+    x.copy <- x
+  }
+  
+
+
+  x.copy@rawBufferedMatrix <- .Call("R_bm_quantile_normalize",x.copy@rawBufferedMatrix,PACKAGE="BufferedMatrixMethods")
+
+  return (x.copy)
+}
+
+
+
+
+bg.correct.BufferedMatrix <- function(x, copy=TRUE){
+
+  
+  if (!is(x,"BufferedMatrix")){
+    stop("Need BufferedMatrix")
+  }
+  
+  if (copy){
+    x.copy <- duplicate(x)
+  } else {
+    x.copy <- x
+  }
+
+  bg.dens <- function(x) {
+    density(x, kernel = "epanechnikov", n = 2^14)
+  }
+
+
+  x.copy@rawBufferedMatrix <- .Call("R_bm_rma_bg_correct", x.copy@rawBufferedMatrix, body(bg.dens), new.env(), PACKAGE="BufferedMatrixMethods")
+
+  return (x.copy)
+
+ 
+}
+
+
+
+
+median.polish.summarize.BufferedMatrix <- function(x,nProbeSets,ProbeNames){
+  
+  return(.Call("R_bm_summarize_medianpolish", x@rawBufferedMatrix, nProbeSets, ProbeNames, PACKAGE="BufferedMatrixMethods"))
+
+}
